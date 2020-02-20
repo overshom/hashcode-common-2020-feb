0148f0661691372cb5305933483ee1ba21097d55
Oleksii Shnyra <shnura94@gmail.com>
2020-02-20 22:23:54 +0200

    feat: file writer

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,31 @@
-import { getInputData } from './file-reader'
+import { getInputData, ILib } from './file-reader'
+import { ILibFlow, createAnswer } from './file-writer'
 
-const run = () => {
-    const data = getInputData('c_incunabula.txt')
-    console.log(data)
+const calcLibEfficiency = (deadline: number, lib: ILib) => {
+    return (deadline - lib.signup_time) / lib.books_per_day
 }
 
-run()
+const run = (file: string) => {
+    let { deadline, libs } = getInputData(file)
+    libs.sort((a, b) => calcLibEfficiency(deadline, b) - calcLibEfficiency(deadline, a))
+    const lib_flow: ILibFlow[] = []
+    for (const l of libs) {
+        deadline -= l.signup_time
+        if (deadline < 1) {
+            break
+        }
+        lib_flow.push({
+            index: l.index,
+            book_order: l.desc_score_books.map(b => b.id),
+        })
+    }
+    createAnswer({ filename: file, lib_flow })
+    console.log('created', file)
+}
+
+// run('a_example.txt')
+// run('b_read_on.txt')
+// run('c_incunabula.txt')
+// run('d_tough_choices.txt')
+// run('e_so_many_books.txt')
+// run('f_libraries_of_the_world.txt')

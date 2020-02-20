c1d949f348d2f2f3edab0bd74e9b82c3ac8c656c
Ihor Tymofieiev <ihor@Ihors-MacBook-Pro.local>
2020-02-20 22:00:54 +0200

    feat: add solution

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,44 @@
 import { getInputData } from './file-reader'
+import { cachedDataVersionTag } from 'v8'
+
+const libAmount = (signUpTime: number, bookPerDay: number, listOfBooks: [{ id: number, score: number }] & any, deadline: number, subDedline: number) => {
+    const workDays = deadline - signUpTime - subDedline;
+    const countOfBook = workDays * bookPerDay;
+    let amount = 0;
+    for(let t = 0; t < countOfBook && t < listOfBooks.length; t++){
+        amount += listOfBooks[t].score;
+    }
+    return amount;
+    };
 
 const run = () => {
-    const data = getInputData('a_example.txt')
-    console.log(data)
+    const data: any = getInputData('a_example.txt')
+    data.libs = data.libs.map((r: any, index: number) => (
+        {
+            lib_id: index,
+            profit: 0,
+            pureDate: r,
+        }))
+const resultDate = [];
+let subIndexAmount = 0;
+
+for(let index = 0; index < data.total_libs; index ++) {
+    const libScoupe = data.libs;
+    data.libs = [];
+    for(let t = 0; t < libScoupe.length; t++){
+        data.libs.push({lib_id: libScoupe[t].lib_id,
+            profit: libAmount(libScoupe[t].pureDate.signup_time, libScoupe[t].pureDate.ship_time,libScoupe[t].pureDate.desc_score_books, data.deadline, subIndexAmount),
+        pureDate: libScoupe[t].pureDate});
+    }
+    data.libs.sort((a: any, b: any) => {
+        return a.profit - b.profit;
+      })
+      const element = data.libs.pop();
+      subIndexAmount += element.pureDate.signup_time;
+      resultDate.push(element);
+}
+
+console.log(resultDate);
 }
 
 run()
